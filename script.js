65a85b6729823b12dc94caeb5fa0cc5660207ea4
haraharu389 <167289025+haraharu389@users.noreply.github.com>
2024-06-05 15:17:34 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript
+// console.log('Hello world!');
+
+  $(function(){
+    $('.bxslider').bxSlider({
+      // オプションを書く場所
+      mode: 'fade',
+      captions: true,
+      auto: true,
+   
+    
+    });
+  });
